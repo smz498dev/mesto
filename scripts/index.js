a103f9b05503f07bcdbb182dce49201f9cd4c9db
smz498dev <vovan.smz@gmail.com>
2023-05-11 17:12:12 +0300

    feat:Реализованое добавление карточек

--- scripts/index.js
+++ scripts/index.js
@@ -1,24 +1,43 @@
-let popup = document.querySelector('.popup');
-let btnEdit = document.querySelector('.profile__info-edit');
-let btnClose = document.querySelector('.popup__close-icon');
-let profileTitle = document.querySelector('.profile__title');
-let profileSubtitle = document.querySelector('.profile__subtitle');
-let inputName = document.querySelector('#input-name');
-let inputJob = document.querySelector('#input-job');
-let formEdit = document.querySelector('.popup__form');
+const popupAdd = document.querySelector('#popupAdd');
+const popupEdit = document.querySelector('#popupEdit');
+const btnEdit = document.querySelector('.profile__info-edit');
+const btnCloseEdit = document.querySelector('#btnCloseEdit');
+const btnCloseAdd = document.querySelector('#btnCloseAdd');
+const profileTitle = document.querySelector('.profile__title');
+const profileSubtitle = document.querySelector('.profile__subtitle');
+const inputName = document.querySelector('#input-name');
+const inputJob = document.querySelector('#input-job');
+const inputNamePlace = document.querySelector('#input-name-place');
+const inputUrlPlace = document.querySelector('#input-url');
+const formEdit = document.querySelector('#popupFormEdit');
+const formAdd = document.querySelector('#popupFormAdd');
+const elements = document.querySelector('.elements');
+const elementTemplate = document.querySelector('#elementTemplate').content;
+const btnAdd = document.querySelector('.profile__btn-add');
 
 // Открытие попапа по клику
-function openPopup() {
-  popup.classList.add('popup_opened');
+
+function openPopupEdit() {
+  popupEdit.classList.add('popup_opened');
   inputName.value = profileTitle.textContent;
   inputJob.value = profileSubtitle.textContent;
 }
-// Закрытие попапа 
-function popupClose() {
-  popup.classList.remove('popup_opened');
+
+function openPopupAdd() {
+  popupAdd.classList.add('popup_opened');
+  
 }
+// Закрытие попапа 
+function closePopupEdit() {
+  popupEdit.classList.remove('popup_opened');
+}
+function closePopupAdd() {
+  popupAdd.classList.remove('popup_opened');
+}
+
+
 //Действие при нажатии на кнопку сохранить
-function saveDataForm(evt) {
+function saveDataFormEdit(evt) {
   evt.preventDefault();
 
   let nameInput = inputName.value;
@@ -26,14 +45,76 @@ function saveDataForm(evt) {
 
   profileTitle.textContent = `${nameInput}`;
   profileSubtitle.textContent = `${jobInput}`;
+  closePopupEdit();
+}
 
-  popupClose();
+
+//Действие при нажатии на кнопку создать (создание и добавление нового места)
+function createNewPlace(evt) {
+  evt.preventDefault();
+
+  let namePlace = inputNamePlace.value;
+  let urlPlace = inputUrlPlace.value;
+  
+  let newCard = {
+    name:`${namePlace}`, 
+    link:`${urlPlace}`
+  };
+
+  initialCards.push(newCard);
+  
+  let newElement = elementTemplate.querySelector('.element').cloneNode(true);
+  newElement.querySelector('.element__img').src = `${initialCards[initialCards.length - 1].link}`;
+  newElement.querySelector('.element__title').textContent = `${initialCards[initialCards.length - 1].name}`;
+  elements.prepend(newElement);
+  newElement.querySelector('.element__like').addEventListener('click', (evt)=> {
+    evt.target.classList.toggle('element__like_active');
+  });
+  newElement.querySelector('.element__remove').addEventListener('click', (evt) => {
+    evt.target.closest('.element').remove();
+  });
+
+  inputNamePlace.value = '';
+  inputUrlPlace.value = '';
+  
+  closePopupAdd();
 }
 
 //listeners 
-btnEdit.addEventListener('click', openPopup);
-btnClose.addEventListener('click', popupClose);
-formEdit.addEventListener('submit', saveDataForm);
+btnEdit.addEventListener('click', openPopupEdit);
+btnAdd.addEventListener('click', openPopupAdd);
+
+btnCloseEdit.addEventListener('click', closePopupEdit);
+btnCloseAdd.addEventListener('click', closePopupAdd);
+
+formEdit.addEventListener('submit', saveDataFormEdit);
+formAdd.addEventListener('submit', createNewPlace);
+
+
+//Добавление карточек из "коробки" при загрузке страницы:
+function addNewCard () {
+initialCards.forEach((item) => {
+  const newElement = elementTemplate.querySelector('.element').cloneNode(true);
+  newElement.querySelector('.element__img').src = `${item.link}`;
+  newElement.querySelector('.element__title').textContent = `${item.name}`;
+  elements.prepend(newElement);
+  newElement.querySelector('.element__like').addEventListener('click', (evt)=> {
+    evt.target.classList.toggle('element__like_active');
+  });
+  newElement.querySelector('.element__remove').addEventListener('click', (evt) => {
+    evt.target.closest('.element').remove();
+  });
+});
+};
+
+addNewCard();
+
+
+
+
+
+
+
 
 
 
