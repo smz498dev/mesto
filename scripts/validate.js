bb318dc9c1ddef94cd1ad7e9b7f1a4c9d4b81863
smz498dev <vovan.smz@gmail.com>
2023-05-26 17:22:23 +0300

    feat: валидация работает, но не правильно

--- scripts/validate.js
+++ scripts/validate.js
@@ -3,12 +3,15 @@ function showError (formElement, inputElement, errorMessage)  {
   inputElement.classList.add('popup__input-item_type_error');
   errorElement.textContent = errorMessage;
   errorElement.classList.add('popup__input-error_active');
+  
+
 };
 function hideError (formElement, inputElement)  {
   const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
   inputElement.classList.remove('popup__input-item_type_error');
   errorElement.classList.remove('popup__input-error_active');
   errorElement.textContent = '';
+  
 };
 
 function checkInputValidity (formElement, inputElement) {
@@ -31,9 +34,14 @@ inputName.addEventListener('input', function () {
 
 function setEventListeners (formElement) {
   const inputList = Array.from(formElement.querySelectorAll('.popup__input-item'));
+  const buttonElement = formElement.querySelector('.popup__save-btn');
+  toggleButtonState(inputList, buttonElement);
+
   inputList.forEach((inputElement) => {
     inputElement.addEventListener('input', function () {
       checkInputValidity(formElement, inputElement);
+      toggleButtonState(inputList, buttonElement);
+      
     })
   });
 } 
@@ -45,8 +53,28 @@ function enableValidation () {
     formElement.addEventListener('submit', (evt) => {
       evt.preventDefault();
     })
-    setEventListeners(formElement);
+    const fieldsetList = Array.from(document.querySelectorAll('.popup__input-container'));
+    fieldsetList.forEach((fieldset) => {
+      setEventListeners(fieldset);
+    });
+    
   });
 }
 
 enableValidation();
+
+function hasInvalidInput (inputList) {
+  return inputList.some((inputElement) => {
+    return inputElement.validity.valid;
+  });
+};
+
+function toggleButtonState (inputList, buttonElement) {
+  if (!hasInvalidInput(inputList)) {
+    buttonElement.classList.add('popup__save-btn_state_inactive');
+    buttonElement.setAttribute('disabled', true);
+  }else{
+    buttonElement.classList.remove('popup__save-btn_state_inactive');
+    buttonElement.removeAttribute('disabled');
+  }
+};
